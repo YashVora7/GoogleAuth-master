49ad81f9f400151521655b19cf0004d3d0e5b9cd
yash <officialyashvora1978@gmail.com>
2023-11-02 10:23:30 +0530

    reset pass via otp

--- index.js
+++ index.js
@@ -30,16 +30,16 @@ app.get("/",async(req,res)=>{
   res.send(data)
 })
 
-app.post("/", (req, res) => {
+app.post("/otp", async(req, res) => {
   console.log(req.body);
   const mail = {
     from: "officialyashvora1978@gmail.com",
     to: req.body.to,
-    subject: req.body.subject,
+    subject: "password reset",
     html: `<h1>Dear Customer use this OTP to Sign In/Up: ${ generateOTP()}</h1>`,
   };
 
-  transport.sendMail(mail, (err, info) => {
+  await transport.sendMail(mail, (err, info) => {
     if (err) {
       console.log(err);
     } else {
@@ -49,6 +49,28 @@ app.post("/", (req, res) => {
 
   res.send("NodeMail is now sent!");
 });
+
+app.post("/verify",async(req,res)=>{
+  let {otp, password, email} = req.body
+
+  if(otp == otp){
+    let userdata = await user.findOne({email: email})
+
+    if(userdata){
+      userdata.password = password
+      await userdata.save()
+      res.send("done")
+    }
+    else{
+      res.send("user not found")
+    }
+
+  }
+  else{
+    res.send("Invalid")
+  }
+
+})
 
 app.listen(8090, () => {
   connect()
